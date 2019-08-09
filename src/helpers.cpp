c985326121b66ab61f1f4e4a0af02ddd8716c7f4
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-09 20:49:27 +0300

    px4 modes + clean

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -15,24 +15,13 @@ MAV_FRAME frame_to_mav_frame(Frame frame)
     switch (frame)
     {
     case FRAME_BODY:
-        /* code */
         return MAV_FRAME_LOCAL_NED;
-        break;
     case FRAME_LOCAL:
-        /* code */
         return MAV_FRAME_LOCAL_NED;
-        break;
-        
     case FRAME_VISION:
-        /* code */
         return MAV_FRAME_VISION_NED;
-        break;
-    
-    
-    default:
-        return MAV_FRAME_LOCAL_NED;
-        break;
     }
+    return MAV_FRAME_LOCAL_NED;
 }
 /*
 
@@ -55,55 +44,48 @@ MAV_FRAME frame_to_mav_frame(Frame frame)
 Mode custom_mode_to_mode(uint32_t mav_mode)
 {
     Mode mode;
-    switch (mav_mode)
+    px4::px4_custom_mode px4_mode;
+    px4_mode.data = mav_mode;
+
+    switch (px4_mode.main_mode)
     {
-    case 0:
+    case px4::PX4_CUSTOM_MAIN_MODE_MANUAL:
         return MODE_MANUAL;
-        break;
-    case 1:
+    case px4::PX4_CUSTOM_MAIN_MODE_ALTCTL:
         return MODE_ALTITUDE;
-        break;
-    case 2:
+    case px4::PX4_CUSTOM_MAIN_MODE_POSCTL:
         return MODE_POSITION;
-        break;
-    case 3:
-        return MODE_MISSION;
-        break;
-    case 4:
-        return MODE_HOLD;
-        break;
-    case 5:
-        return MODE_RETURN;
-        break;
-    case 6:
+    case px4::PX4_CUSTOM_MAIN_MODE_ACRO:
         return MODE_ACRO;
-        break;
-    case 7:
+    case px4::PX4_CUSTOM_MAIN_MODE_OFFBOARD:
         return MODE_OFFBOARD;
-        break;
-    case 8:
+    case px4::PX4_CUSTOM_MAIN_MODE_STABILIZED:
         return MODE_STABILIZE;
-        break;
-    case 9:
+    case px4::PX4_CUSTOM_MAIN_MODE_RATTITUDE:
         return MODE_RATTITUDE;
-        break;
-    case 10:
-        return MODE_TAKEOFF;
-        break;
-    case 11:
-        return MODE_LAND;
-        break;
-    case 12:
-        return MODE_FOLLOW_ME;
-        break;
     }
-    return mode;
+    switch (px4_mode.sub_mode)
+    {
+    case px4::PX4_CUSTOM_SUB_MODE_AUTO_MISSION:
+        return MODE_MISSION;
+    case px4::PX4_CUSTOM_SUB_MODE_AUTO_LOITER:
+        return MODE_HOLD;
+    case px4::PX4_CUSTOM_SUB_MODE_AUTO_RTL:
+        return MODE_RETURN;
+    case px4::PX4_CUSTOM_SUB_MODE_AUTO_TAKEOFF:
+        return MODE_TAKEOFF;
+    case px4::PX4_CUSTOM_SUB_MODE_AUTO_LAND:
+        return MODE_LAND;
+    }
+    return MODE_UNASSIGNED;
+    // return mode;
 }
 Mode base_mode_to_mode(uint8_t mav_mode)
 {
     return MODE_UNASSIGNED;
 }
 
+/*
 bool base_mode_to_arm(uint8_t mav_mode)
 {
     bool armed = false;
@@ -127,16 +109,20 @@ bool base_mode_to_arm(uint8_t mav_mode)
     }
     return armed;
 }
+*/
 
 // tostring
-string PointXYZ::ToString(){
+string PointXYZ::ToString()
+{
     return "[" + to_string(x) + " " + to_string(y) + " " + to_string(z) + "]";
 }
-string RotationRPY::ToString(){
+string RotationRPY::ToString()
+{
     return "[" + to_string(roll) + " " + to_string(pitch) + " " + to_string(yaw) + "]";
 }
-string PointXYZyaw::ToString(){
-    return "[" + to_string(x) + " " + to_string(y) + " " + to_string(z) + " " + to_string(yaw)  + "]";
+string PointXYZyaw::ToString()
+{
+    return "[" + to_string(x) + " " + to_string(y) + " " + to_string(z) + " " + to_string(yaw) + "]";
 }
 
 string mode_ToString(Mode mode)
@@ -146,85 +132,73 @@ string mode_ToString(Mode mode)
     {
     case MODE_MANUAL:
         return "MODE_MANUAL";
-        break;
     case MODE_ALTITUDE:
         return "MODE_ALTITUDE";
-        break;
     case MODE_POSITION:
         return "MODE_POSITION";
-        break;
     case MODE_MISSION:
         return "MODE_MISSION";
-        break;
     case MODE_HOLD:
         return "MODE_HOLD";
-        break;
     case MODE_RETURN:
         return "MODE_RETURN";
-        break;
     case MODE_ACRO:
         return "MODE_ACRO";
-        break;
     case MODE_OFFBOARD:
         return "MODE_OFFBOARD";
-        break;
     case MODE_STABILIZE:
         return "MODE_STABILIZE";
-        break;
     case MODE_RATTITUDE:
         return "MODE_RATTITUDE";
-        break;
     case MODE_TAKEOFF:
         return "MODE_TAKEOFF";
-        break;
     case MODE_LAND:
         return "MODE_LAND";
-        break;
     case MODE_FOLLOW_ME:
         return "MODE_FOLLOW_ME";
-        break;
     case MODE_UNASSIGNED:
         return "MODE_UNASSIGNED";
-        break;
     }
     return "MODE_UNASSIGNED";
     // return mode;
 }
 
-string Telemetry::ToString(){
+string Telemetry::ToString()
+{
     // return
     string str_out = "Telemetry: \n";
     str_out += " Position: " + position.ToString() + "\n";
     str_out += " Velocity: " + velocity.ToString() + "\n";
     str_out += " Rotation: " + rotation.ToString() + "\n";
-    str_out += " Mode: "     + mode_ToString(mode)  + "\n";
-    str_out += " Armed: "    + to_string(armed)    + "\n";
-    str_out += " Connected: "+ to_string(connected)+ "\n";
+    str_out += " Mode: " + mode_ToString(mode) + "\n";
+    str_out += " Armed: " + to_string(armed) + "\n";
+    str_out += " Connected: " + to_string(connected) + "\n";
     return str_out;
 }
 
 inline double hypot(double x, double y, double z)
 {
-	return std::sqrt(x * x + y * y + z * z);
+    return std::sqrt(x * x + y * y + z * z);
 }
 
+float get_dist(PointXYZ p1, PointXYZ p2)
+{
 
-float get_dist(PointXYZ p1, PointXYZ p2){
-
-	return hypot(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
-
+    return hypot(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
 }
 
 // enu ned
 
-PointXYZ enu_to_ned(PointXYZ enu){
+PointXYZ enu_to_ned(PointXYZ enu)
+{
     PointXYZ ned;
     ned.x = enu.y;
     ned.y = enu.x;
     ned.z = -enu.z;
     return ned;
 };
-PointXYZ ned_to_enu(PointXYZ ned){
+PointXYZ ned_to_enu(PointXYZ ned)
+{
     PointXYZ enu;
     enu.x = ned.y;
     enu.y = ned.x;
@@ -232,7 +206,8 @@ PointXYZ ned_to_enu(PointXYZ ned){
     return enu;
 };
 
-PointXYZyaw enu_to_ned(PointXYZyaw enu){
+PointXYZyaw enu_to_ned(PointXYZyaw enu)
+{
     PointXYZyaw ned;
     ned.x = enu.y;
     ned.y = enu.x;
@@ -240,7 +215,8 @@ PointXYZyaw enu_to_ned(PointXYZyaw enu){
     ned.yaw = enu.yaw;
     return ned;
 };
-PointXYZyaw ned_to_enu(PointXYZyaw ned){
+PointXYZyaw ned_to_enu(PointXYZyaw ned)
+{
     PointXYZyaw enu;
     enu.x = ned.y;
     enu.y = ned.x;
